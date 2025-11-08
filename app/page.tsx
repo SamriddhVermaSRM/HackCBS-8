833ecd0e85c1bf3e7d8e1a59c9563aa8a92a4246
divyansh-1214 <divyansh.srri23@gmail.com>
2025-11-08 19:00:53 +0530

    feat: home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 "use client";
-
+import { useEffect } from "react"
+import Lenis from "@studio-freight/lenis"
+import Hero from "@/components/hero"
+import Featured from "@/components/featured"
+import Promo from "@/components/promo"
+import Footer from "@/components/footer"
 export default function Home() {
+    useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time:any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
   return (
     <>
-      <main>
-        <h1>Path: 21474836886</h1>
+      <main className=" ">
+        <Hero/>
+        <Featured/>
+        <Promo/>
+        <Featured/>
       </main>
     </>
   );
