7693ad3161a908b5017a2e4aca1ebdd5fd33c6f3
Samriddh Pratap Verma <samriddh_be22it132@srmcem.ac.in>
2025-11-08 18:48:20 +0530

    modules api made

---
+++ app/api/module/[module]/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+import modules from "@/data/modules.json";
+
+export async function POST(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const moduleId = pathname.split("/").pop();
+  const { lesson_id } = await req.json();
+
+  const moduleLesson =
+    modules[moduleId as keyof typeof modules].lessons[lesson_id as number];
+  if (!moduleLesson) {
+    return Response.json({ error: "Lesson not found" }, { status: 404 });
+  }
+
+  return Response.json(moduleLesson);
+}
---
+++ app/api/module/route.tsx
@@ -0,0 +1,22 @@
+import modules from "@/data/modules.json";
+import { NextResponse } from "next/server";
+
+export function GET() {
+  const moduleDescription = Object.entries(modules).map((o) => ({
+    module_id: o[0],
+    name: o[1].name,
+    description: o[1].description,
+  }));
+  return NextResponse.json(moduleDescription);
+}
+
+export async function POST(req: NextResponse) {
+  const { module_id, index } = await req.json();
+
+  const moduleLesson =
+    modules[module_id as keyof typeof modules].lessons[index as number];
+  if (!module) {
+    return NextResponse.json({ error: "Module not found" }, { status: 404 });
+  }
+  return NextResponse.json(moduleLesson);
+}
