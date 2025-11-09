e62174cdaaea3c953fab8ea7a65b9f3862dfe963
Samriddh Pratap Verma <samriddh_be22it132@srmcem.ac.in>
2025-11-09 11:52:32 +0530

    fix

--- lib/Ai.tsx
+++ lib/Ai.tsx
@@ -1,7 +1,7 @@
-import { GoogleGenAI } from '@google/genai';
+import { GoogleGenAI } from "@google/genai";
 
 export const Gemini = new GoogleGenAI({
-	apiKey: process.env.GENAI_API_KEY,
+  apiKey: AIzaSyBRwrN8A4J6kFKnbWlK0cr058AFpzmkON4,
 });
 
 export const MAX_AUDIO_LENGTH = 10;
